d67c53508365076c975de5cee661a9cd7de884d7
Marco Dalla G <marco.dallagiacoma@gmail.com>
2016-02-07 16:55:40 +0200

    added email registration

--- server.py
+++ server.py
@@ -5,5 +5,12 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/register-email/', methods=['POST'])
+def register_email():
+    email = request.form['email']
+    with open('data/emails.txt', 'a') as f:
+        f.write(email + '\n')
+    return 'ok'
+
 if __name__ == '__main__':
     app.run(debug=True)
