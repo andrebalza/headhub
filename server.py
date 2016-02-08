6090a4f01c0b117224fcf8964d454f2ac52ecf67
Marco Dalla G <marco.dallagiacoma@gmail.com>
2016-02-08 15:26:11 +0200

    better email validation

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import re
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
@@ -5,10 +6,19 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+
+def validate_email(raw_email):
+    email = raw_email.strip()
+    regex = re.compile(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
+    if regex.match(email):
+        return email
+    return None
+
+
 @app.route('/register-email/', methods=['POST'])
 def register_email():
-    email = request.form['email']
-    if '@' in email and not '\n' in email and not ' ' in email.strip():
+    email = validate_email(request.form['email'])
+    if email:
         with open('data/emails.txt', 'a') as f:
             f.write(email + '\n')
     return 'ok'
